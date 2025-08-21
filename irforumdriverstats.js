150ba32cb603b68bad8951ecdb35c3a3867f70d6
mrg21 <mrogin@web.de>
2025-08-21 22:10:53 +0200

    Update irforumdriverstats.js v1.31
    add simracer-tools.com/seasonstandings

--- irforumdriverstats.js
+++ irforumdriverstats.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         iR Forum user stats
 // @namespace    http://tampermonkey.net/
-// @version      1.30_2025-06-14
+// @version      1.31_2025-08-21
 // @description  Show user stats in the iRacing forum
 // @author       MR
 // @match        https://forums.iracing.com/*
@@ -134,7 +134,8 @@ if ((document.documentElement.clientWidth, window.innerWidth || 0) * 1.3 < (docu
                 '<a target="_blank" href="https://nyoom.app/search/'+ driver.cust_id +'" class="driver-link"> NYOOM </a> &nbsp; '+
                 '<a target="_blank" href="https://www.irstats.net/driver/'+ driver.cust_id +'" class="driver-link"> iRStats </a> &nbsp; '+
 				'<a target="_blank" href="https://iracingdata.com/user/careerstats/'+ driver.cust_id +'" class="driver-link"> iRdata </a> &nbsp; '+
-                '<a target="_blank" href="https://season-summary.dyczkowski.dev/driver/'+ driver.cust_id +'?category=sports_car" class="driver-link"> Season </a> &nbsp; '+
+                '<a target="_blank" href="https://season-summary.dyczkowski.dev/driver/'+ driver.cust_id +'?category=sports_car" class="driver-link"> SSummary </a> &nbsp; '+
+                '<a target="_blank" href="https://simracer-tools.com/seasonstandings/?driver='+ driver.cust_id +'&stats=1" class="driver-link"> SStandings </a> &nbsp; '+
                 '<a target="_blank" href="https://members-ng.iracing.com/web/racing/results-stats/results"'+
                 ' onclick="navigator.clipboard.writeText('+ driver.cust_id +');"'+
                 ' class="driver-link"> Results </a> &nbsp;';
